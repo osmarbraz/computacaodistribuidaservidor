59b6e692b5c9d823affca308ffcc4ef98a62e8b2
admin <admin@OSMARVM0>
2017-11-03 20:50:49 -0200

    Alteração para incluir o endereço e porta editavel do servidor

--- Cliente.java
+++ Cliente.java
@@ -9,6 +9,9 @@ import java.io.Serializable;
 
 /**
  * Classe que representa a abstração principal do sistema.
+ *
+ * @author osmarbraz
+ * @version 1.0
  */
 public class Cliente implements Serializable {
 
@@ -35,9 +38,8 @@ public class Cliente implements Serializable {
     /**
      * Construtor com argumentos da classe.
      *
-     * @param clienteId id de um cliente
-     * @param nome nome de um cliente
-     * @param cpf cpf de um cliente
+     * @param clienteId
+     * @param nome
      */
     public Cliente(String clienteId, String nome, String cpf) {
         setClienteId(clienteId);
@@ -47,8 +49,6 @@ public class Cliente implements Serializable {
 
     /**
      * Retorna o id de um cliente.
-     *
-     * @return uma string com o id do cliente
      */
     public String getClienteId() {
         return clienteId;
@@ -65,8 +65,6 @@ public class Cliente implements Serializable {
 
     /**
      * Retorna o nome de um cliente.
-     *
-     * @return Uma string com o nome
      */
     public String getNome() {
         return nome;
@@ -83,8 +81,6 @@ public class Cliente implements Serializable {
 
     /**
      * Retorna o cpf de um cliente.
-     *
-     * @return Uma strint com o cpf
      */
     public String getCpf() {
         return cpf;
@@ -101,59 +97,32 @@ public class Cliente implements Serializable {
 
     /**
      * Retorna uma string com o estado do objeto.
-     *
-     * @return Uma string com os dados do cliente concatenados
      */
     public String paraString() {
         return ("clienteId:" + getClienteId() + " - Nome :" + getNome() + " = CPF:" + getCpf());
     }
 
     /**
-     * Chamada da operação inserir para o servidor default
-     *
-     * @return um boolean com o resultado da operação
+     * Persiste um objeto.
      */
     public boolean inserir() {
-        return inserir("localhost", 1099);
-    }
-
-    /**
-     * Persiste um objeto.
-     *
-     * @param enderecoServidor Endereço do servidor remoto
-     * @param portaServidor Porta do servidor remoto
-     * @return um boolean com o resultado da operação
-     */
-    public boolean inserir(String enderecoServidor, int portaServidor) {
         try {
-            DAOFactory factory = DAOFactory.getDAOFactory(enderecoServidor, portaServidor);
+            DAOFactory factory = DAOFactory.getDAOFactory();
             ClienteDAO clientedao = factory.getClienteDAO();
             return clientedao.inserir(this);
         } catch (RemoteException e) {
             System.out.println("Erro no servidor: " + e);
             return false;
         }
-    }
 
-    /**
-     * Chamada da operação alterar para o servidor default
-     *
-     * @return um int com o resultado da operação
-     */
-    public int alterar() {
-        return alterar("localhost", 1099);
     }
 
     /**
      * Altera o estado de um objeto persistente.
-     *
-     * @param enderecoServidor Endereço do servidor remoto
-     * @param portaServidor Porta do servidor remoto
-     * @return Um inteiro com a quantidade de alterações
      */
-    public int alterar(String enderecoServidor, int portaServidor) {
+    public int alterar() {
         try {
-            DAOFactory factory = DAOFactory.getDAOFactory(enderecoServidor, portaServidor);
+            DAOFactory factory = DAOFactory.getDAOFactory();
             ClienteDAO clientedao = factory.getClienteDAO();
             return clientedao.alterar(this);
         } catch (RemoteException e) {
@@ -163,24 +132,11 @@ public class Cliente implements Serializable {
     }
 
     /**
-     * Chamada da operação excluir para o servidor default
-     *
-     * @return um int com o resultado da operação
+     * Exclui um objeto da persistência através do identificado.
      */
     public int excluir() {
-        return excluir("localhost", 1099);
-    }
-
-    /**
-     * Exclui um objeto da persistência através do id do cliente.
-     *
-     * @param enderecoServidor Endereço do servidor remoto
-     * @param portaServidor Porta do servidor remoto
-     * @return
-     */
-    public int excluir(String enderecoServidor, int portaServidor) {
         try {
-            DAOFactory factory = DAOFactory.getDAOFactory(enderecoServidor, portaServidor);
+            DAOFactory factory = DAOFactory.getDAOFactory();
             ClienteDAO clientedao = factory.getClienteDAO();
             return clientedao.excluir(this);
         } catch (RemoteException e) {
@@ -190,26 +146,13 @@ public class Cliente implements Serializable {
     }
 
     /**
-     * Chamada da operação aplicar filtro para o servidor default
-     *
-     * @return um List com o resultado da operação
-     */
-    public List aplicarFiltro() {
-        return aplicarFiltro("localhost", 1099);
-    }
-
-    /**
      * Retorna uma lista de objetos que atende os valores passados pelo objeto.
      * O Id realiza comparação e o nome realiza uma comparação parcial.
-     *
-     * @param enderecoServidor Endereço do servidor remoto
-     * @param portaServidor Porta do servidor remoto
-     * @return
      */
     @SuppressWarnings("rawtypes")
-    public List aplicarFiltro(String enderecoServidor, int portaServidor) {
+    public List aplicarFiltro() {
         try {
-            DAOFactory factory = DAOFactory.getDAOFactory(enderecoServidor, portaServidor);
+            DAOFactory factory = DAOFactory.getDAOFactory();
             ClienteDAO clientedao = factory.getClienteDAO();
             return clientedao.aplicarFiltro(this);
         } catch (RemoteException e) {
@@ -219,25 +162,12 @@ public class Cliente implements Serializable {
     }
 
     /**
-     * Chamada da operação getLista para o servidor default
-     *
-     * @return um List com o resultado da operação
-     */
-    public List getLista() {
-        return getLista("localhost", 1099);
-    }
-
-    /**
      * Retorna uma lista com todos os objetos.
-     *
-     * @param enderecoServidor Endereço do servidor remoto
-     * @param portaServidor Porta do servidor remoto
-     * @return
      */
     @SuppressWarnings("rawtypes")
-    public List getLista(String enderecoServidor, int portaServidor) {
+    public List getLista() {
         try {
-            DAOFactory factory = DAOFactory.getDAOFactory(enderecoServidor, portaServidor);
+            DAOFactory factory = DAOFactory.getDAOFactory();
             ClienteDAO clientedao = factory.getClienteDAO();
             return clientedao.getLista();
         } catch (RemoteException e) {
@@ -247,25 +177,12 @@ public class Cliente implements Serializable {
     }
 
     /**
-     * Chamada da operação abrir para o servidor default
-     *
-     * @return um boolean com o resultado da operação
-     */
-    public boolean abrir() {
-        return abrir("localhost", 1099);
-    }
-
-    /**
      * Restaura o estado do objeto apartir do id.
-     *
-     * @param enderecoServidor
-     * @param portaServidor
-     * @return
      */
     @SuppressWarnings("rawtypes")
-    public boolean abrir(String enderecoServidor, int portaServidor) {
+    public boolean abrir() {
         try {
-            DAOFactory factory = DAOFactory.getDAOFactory(enderecoServidor, portaServidor);
+            DAOFactory factory = DAOFactory.getDAOFactory();
             ClienteDAO clientedao = factory.getClienteDAO();
             List lista = clientedao.aplicarFiltro(this);
             if (!lista.isEmpty()) {
@@ -283,11 +200,11 @@ public class Cliente implements Serializable {
     }
 
     /**
-     * Retorna uma cópia do cliente com os mesmos dados
+     * Retorna uma copia do cliente com os mesmos dados
      *
      * @return uma nova instancia do objeto cliente com os mesmos dados
      */
     public Cliente copia() {
-        return new Cliente(getClienteId(), getNome(), getCpf());
+        return new Cliente(getClienteId(), getNome() + "_1", getCpf());
     }
 }
--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -9,18 +9,22 @@ public abstract class DAOFactory {
 
     //Tipos de Fonte de Dados suportados pela Factory
     public static final int HASHMAP = 1;
+	
+	private static String enderecoServidor;
+	private static int portaServidor;
 
-    /**
-     * Retorna o DAO instanciado da fábrica
-     */
+    //Retorna o DAO instanciado
     public abstract ClienteDAO getClienteDAO();
+	
+	//Retorna a Factory do tipo especificado default	
+    public static DAOFactory getDAOFactory() {		
+		return getDAOFactory("localhost", 1099);
+	}	
 
-    /**
-     * Retorna a Factory do tipo especificado
-     *
-     * @return Uma factory a ser utilizada
-     */
-    public static DAOFactory getDAOFactory() {
+    //Retorna a Factory do tipo especificado	
+    public static DAOFactory getDAOFactory(String enderecoServidor, int portaServidor) {
+		setEnderecoServidor(enderecoServidor);
+		setPortaServidor(portaServidor);
         int whichFactory = Factory.FABRICA;
         switch (whichFactory) {
             case HASHMAP:
@@ -30,4 +34,20 @@ public abstract class DAOFactory {
                 return null;
         }
     }
+	
+	public static void setEnderecoServidor(String _enderecoServidor){
+		enderecoServidor = _enderecoServidor;
+	}
+	
+	public static String getEnderecoServidor(){
+		return enderecoServidor;
+	}
+	
+	public static void setPortaServidor(int _portaServidor){
+		portaServidor = _portaServidor;
+	}
+	
+	public static int getPortaServidor(){
+		return portaServidor;
+	}	
 }
